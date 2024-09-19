a18c7f56859716b0bd3bec19003fb3b9f14723a7
FunsizedWrath <151151740+FunsizedWrath@users.noreply.github.com>
2024-09-19 15:11:54 +0200

    added pawn to Charlie's scene

--- Assets/main_menu.cs
+++ Assets/main_menu.cs
@@ -7,7 +7,7 @@ public class main_menu : MonoBehaviour
 {
     public void PlayGame ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Charlie");
     }
 
     public void QuitGame ()
